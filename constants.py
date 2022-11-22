4a71af47c794a69217fc6f5421020fe88b1826ea
Jew1337 <76224645+Aunapu-Georg@users.noreply.github.com>
2022-11-22 09:16:55 +0000

    Добавил модуль с константами

---
+++ constants.py
@@ -0,0 +1,20 @@
+# Фундаментальные физические постоянные
+
+planck_constant = 6.62607 * 10 ** (-34) # Дж * с
+speed_of_light_in_vacuum = 299792456 # м / c
+gravitational_constant = 6.674 * 10 ** (-11) # м^3 / кг * с^2 
+elementary_charge = 1.602176634 * 10 ** (-19) # Кл
+boltzmann_constant = 1.380649 * 10 ** (-23) # Дж / К
+
+# Постоянные, связывающие разные системы единиц
+
+atomic_mass_unit = 1.660539066 * 10 ** (-27) # кг
+avogadro_constant = 6.02214076 * 10 ** (23) # 1 / моль
+universal_gas_constant = boltzmann_constant * avogadro_constant # Дж / К * моль
+standard_atmospheric_pressure = 101325 # Па
+
+# Массы элементарных частиц
+
+electron_mass = 9.1093837015 * 10 ** (-31) # кг
+proton_mass = 1.67262192369 * 10 ** (-27) # кг
+neutron_mass = 1.67492749804 * 10 ** (-27) # кг
