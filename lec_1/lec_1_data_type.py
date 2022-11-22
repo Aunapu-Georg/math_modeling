4d6e4d7d748f2552a82964b8847f657bf84c43a1
Jew1337 <76224645+Aunapu-Georg@users.noreply.github.com>
2022-11-22 08:55:46 +0000

    Сделал полностью все задачи и лекцию

---
+++ lec_1/lec_1_data_type.py
@@ -0,0 +1,19 @@
+type(3)
+
+print(type(3))
+
+print(type(3.4))
+
+print(type('Молодец'))
+
+a = 'Molodec'
+print(type(a))
+
+a = ['Molocdec', 1, 'Круто', 6, 8]
+print(type(a))
+
+print(type(True))
+
+print(type(False))
+
+print(type(None))
